49f929c5510ee9a70fd1dd6bc0a496c3056c09c7
chaiyaphat supharak <chaiyaphat2024@gmail.com>
2025-03-30 14:57:08 +0700

    fix: remove unused code

--- src/types/todo/todo.schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const TodoSchema = z.object({
-  title: z.string().min(1, { message: "Title is required" }),
-  description: z.string().optional(),
-});
-
-export type TodoType = z.infer<typeof TodoSchema>;
